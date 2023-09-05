4afc56d76ac0c5e2897701de1e6f20a9c0fd43d7
jhs <jsp7406@naver.com>
2023-09-05 10:49:52 +0900

    feat: "0905_오전"

--- src/main/java/servlet/signinServlet.java
+++ src/main/java/servlet/signinServlet.java
@@ -1,8 +1,10 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import data.UserData;
 import entity.User;
+import security.Authentication;
+import security.SecurityContextHolder;
 import utils.JsonParseUtil;
 import utils.ResponseUtil;
 
@@ -22,11 +26,16 @@ public class signinServlet extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Map<String, Object> signinUser = JsonParseUtil.toMap(request.getInputStream());
-		Boolean responseData = false;
+		Map<String, String> responseData = new HashMap<>();
+		
+		System.out.println(signinUser);
 	
 		for(User user : UserData.userList) {
 			if(Objects.equals(user.getUsername(), signinUser.get("username")) && Objects.equals(user.getPassword(), signinUser.get("password"))) {
-				responseData = true;
+				
+				String token = UUID.randomUUID().toString();
+				SecurityContextHolder.addAuth(new Authentication(user, token));
+				responseData.put("token", token);
 				break;
 			}
 		}
